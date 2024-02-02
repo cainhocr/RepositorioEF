963ce5baba1260bcd8a2bed2d2d5531df0f07391
CAIO IGNACIO <caio.ignacio@portalsesisp.org.br>
2024-02-02 17:05:35 -0300

    projeto parcial

--- EFExemplo/EFExemplo/Data/EFExemploContext.cs
+++ EFExemplo/EFExemplo/Data/EFExemploContext.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFExemplo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFExemplo.Data
 {
     public class EFExemploContext : DbContext
     {
         public EFExemploContext(DbContextOptions<EFExemploContext> options): base(options){ }
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+        }
 
-        
+
     }
 }
